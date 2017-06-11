b07eb96753a7ee6b83ba7dd063e5e6af42eb800e
NemoNguyen <nemonguyen@Quocs-MacBook-Pro.local>
2017-06-11 16:34:31 +0800

    Remove create gridview adapter every time
    So we can reuse aadapter, to keep selected that user scroll

--- HDWallpaperApp/gen/com/csform/android/wallpaper/BuildConfig.java
+++ HDWallpaperApp/gen/com/csform/android/wallpaper/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.nemostation.android.fullnarutowallpaper;
+package com.nemostation.android.fullnarutoartwallpaper;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
